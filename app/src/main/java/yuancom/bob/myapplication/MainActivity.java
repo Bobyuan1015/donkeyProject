957fa327364c64736ff8fe133d2e443d0cf922d7
Bobyuan1015 <Bobyuan1015@gmail.com>
2017-07-25 00:02:51 +0100

    add Tag for MainActivity

--- app/src/main/java/yuancom/bob/myapplication/MainActivity.java
+++ app/src/main/java/yuancom/bob/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    final String Tag = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
